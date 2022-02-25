2b112ecb6cc92301bb8519d8463f1f56a9bab158
Krzysztof Repsch <krzys.repsch@gmail.com>
2022-02-25 23:22:29 +0100

    Improved Questions test
    Created function getAllQuestions and now by passing bool
    I'm checking toggle between all questions and answers

--- src/views/Faq/components/__tests__/Questions.test.tsx
+++ src/views/Faq/components/__tests__/Questions.test.tsx
@@ -4,33 +4,39 @@ import 'intersection-observer';
 import Question from '../Question';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'assets/styles/theme';
+import { faqData } from 'data/faqData';
 
-type MockQuestionProps = {
-    question: string,
-    answer: string
-}
-
-const MockQuestion = ({ question, answer }: MockQuestionProps) => {
+const MockQuestion = () => {
   return (
     <ThemeProvider theme={theme}>
+      {faqData.map(({question, answer}) => (
         <Question  question={question} answer={answer} />
+      ))}
     </ThemeProvider>
   );
 };
 
-describe('Question', () => {
-    it('shoud show answer after question beeing clicked', () => {
-      render(<MockQuestion question='Ile kosztuje Maltańczyk?' answer='Cena pieska jest ustalana indywidualnie.'/>);
-      const faqQuestionElement = screen.getByText('Ile kosztuje Maltańczyk?');
-      const faqAnswerElement = screen.getByText('Cena pieska jest ustalana indywidualnie.');
-      fireEvent.click(faqQuestionElement)
-      expect(faqAnswerElement).toHaveClass('toggle')
-    });
-    it('shoud hide answer after question beeing clicked for the second time', () => {
-      render(<MockQuestion question='Ile kosztuje Maltańczyk?' answer='Cena pieska jest ustalana indywidualnie.'/>);
-      const faqQuestionElement = screen.getByText('Ile kosztuje Maltańczyk?');
-      const faqAnswerElement = screen.getByText('Cena pieska jest ustalana indywidualnie.');
+const getAllQuestions = (isDubleClick: boolean) => {
+  faqData.forEach(({ question, answer }) => {
+    const faqQuestionElement = screen.getByText(question);
+    const faqAnswerElement = screen.getByText(answer);
+    if(isDubleClick){
       fireEvent.doubleClick(faqQuestionElement)
       expect(faqAnswerElement).not.toHaveClass('toggle')
+    } else {
+      fireEvent.click(faqQuestionElement)
+      expect(faqAnswerElement).toHaveClass('toggle')
+    }
+  })
+}
+
+describe('Question', () => {
+    it('shoud show answer after question beeing clicked', () => {
+      render(<MockQuestion />);
+      getAllQuestions(false)
+    });
+    it('shoud hide answer after question beeing clicked for the second time', () => {
+      render(<MockQuestion />);
+      getAllQuestions(true)
     });
   });
